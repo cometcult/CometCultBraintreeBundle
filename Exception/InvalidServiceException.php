22b039bd70d4565721a68f6877e9904dcec8f6ce
karolsojko <zoja87@gmail.com>
2013-05-07 09:19:21 +0200

    Added phpDoc

--- Exception/InvalidServiceException.php
+++ Exception/InvalidServiceException.php
@@ -4,6 +4,9 @@ namespace CometCult\BraintreeBundle\Exception;
 
 use Exception;
 
+/**
+ * Exception used for indicating invalid service creation
+ */
 class InvalidServiceException extends Exception
 {
 }
